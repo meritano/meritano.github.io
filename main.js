253752fe3dbe680a97026b89ea2bcc756929fd00
meritano <91009804+meritano@users.noreply.github.com>
2021-09-19 12:49:32 +0200

    commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var moved = false;
+$(function(){
+    $(".moving-button").on({
+        mouseover:function(){
+        	if(moved === true) {
+          	$(this).css({left:"0px"});
+          } else {
+          	$(this).css({left:"65px"});
+          }
+          moved = !moved;
+        }
+    });
+});
+
+function onNoClick() {
+    alert("Allora mangia la merda!");
+}
